2bf02fec3ae70d8308db0516ab794420b5828a13
Artem <dzbarts@mail.ru>
2021-09-14 14:29:50 +0300

    add some files from previous project

---
+++ CORE/Indicators.py
@@ -0,0 +1,191 @@
+import math
+import pandas_datareader as web
+import datetime as dt
+
+
+# usually close data
+
+# Simple moving average
+# https://en.wikipedia.org/wiki/Moving_average
+def SMA(data, period):
+    if len(data) == 0:
+        raise Exception("Empty data")
+    if period <= 0:
+        raise Exception("Invalid period")
+
+    interm = 0
+    result = []
+    for i, v in enumerate(data):
+        interm += v
+        if (i + 1) < period:
+            result.append(math.nan)
+        else:
+            result.append(interm / float(period))
+            interm -= data[i + 1 - period]
+    return result
+
+
+# Exponential moving average
+# https://en.wikipedia.org/wiki/Moving_average#Exponential_moving_average
+def EMA(data, period):
+    if period <= 1:
+        raise Exception("Invalid period")
+
+    sma = SMA(data, period)
+    multiplier = 2 / (float(period + 1))
+    result = []
+
+    for k, v in enumerate(sma):
+        if math.isnan(v):
+            result.append(math.nan)
+        else:
+            prev = result[k - 1]
+            if math.isnan(prev):
+                result.append(v)
+                continue
+            ema = (data[k] - prev) * multiplier + prev
+            result.append(ema)
+    return result
+
+
+# Moving average convergence/divergence
+# https://en.wikipedia.org/wiki/MACD
+# usually take 12,26,9, but u can take 5,35,5 for more risky trading
+def MACD(data, shortperiod, longperiod, signalperiod):
+    macd, macdsignal, macdhist = [], [], []
+
+    short_ema = EMA(data, shortperiod)
+    long_ema = EMA(data, longperiod)
+
+    diff = []
+
+    for k, short in enumerate(short_ema):
+        if math.isnan(short) or math.isnan(long_ema[k]):
+            macd.append(math.nan)
+            macdsignal.append(math.nan)
+        else:
+            macd.append(short - long_ema[k])
+            diff.append(macd[k])
+
+    diff_ema = EMA(diff, signalperiod)
+    macdsignal += diff_ema
+
+    for k, ms in enumerate(macdsignal):
+        if math.isnan(ms) or math.isnan(macd[k]):
+            macdhist.append(math.nan)
+        else:
+            macdhist.append(macd[k] - macdsignal[k])
+
+    return macd, macdsignal, macdhist
+
+
+# Relative strength index
+# https://en.wikipedia.org/wiki/Relative_strength_index
+def RSI(data, period):
+    u_days = []
+    d_days = []
+
+    for i, _ in enumerate(data):
+        if i == 0:
+            u_days.append(0)
+            d_days.append(0)
+        else:
+            if data[i] > data[i - 1]:
+                u_days.append(data[i] - data[i - 1])
+                d_days.append(0)
+            elif data[i] < data[i - 1]:
+                d_days.append(data[i - 1] - data[i])
+                u_days.append(0)
+            else:
+                u_days.append(0)
+                d_days.append(0)
+
+    ema_u = EMA(u_days, period)
+    ema_d = EMA(d_days, period)
+
+    result = []
+
+    for k, _ in enumerate(data):
+        if ema_d[k] == 0:
+            result.append(100)
+        else:
+            result.append(100 - (100 / (1 + ema_u[k] / ema_d[k])))
+
+    return result
+
+
+# Double Exponential Moving Average
+# https://en.wikipedia.org/wiki/Exponential_smoothing#Double_exponential_smoothing
+def DEMA(data, period):
+    ema = EMA(data, period)
+    ema_slice = ema[(period - 1):]
+    ema2 = EMA(ema_slice, period)
+    ema2 = [math.nan] * (period - 1) + ema2
+    e2 = list(map(lambda x: x * 2, ema))
+
+    result = []
+
+    for i in range(len(data)):
+        result.append(e2[i] - ema2[i])
+    return result
+
+
+# Triple Exponential Moving Average
+def TEMA(data, period):
+    e1 = EMA(data, period)
+    e1_slice = e1[(period - 1):]
+    e2 = EMA(e1_slice, period)
+    e2_slice = e2[(period - 1):]
+    e3 = EMA(e2_slice, period)
+    e2 = [math.nan] * (period - 1) + e2
+    e3 = [math.nan] * 2 * (period - 1) + e3
+
+    e1 = list(map(lambda x: x * 3, e1))
+    e2 = list(map(lambda x: x * 3, e2))
+
+    result = []
+    for i in range(len(data)):
+        result.append(e1[i] - e2[i] + e3[i])
+
+    return result
+
+
+def Bulls_power(data, period):
+    result = data['High'] - EMA(data['Close'], period)
+    return result
+
+
+def Bears_power(data, period):
+    result = data['Low'] - EMA(data['Close'], period)
+    return result
+
+
+def Elder_Rays(data, period):
+    ema = EMA(data['Close'], period)
+    bulls = Bulls_power(data, period)
+    bears = Bears_power(data, period)
+    return ema, bulls, bears
+
+
+# https://ru.wikipedia.org/wiki/Индекс_массы_(технический_анализ)
+def MI(data, period):
+    dataHL = data['High'] - data['Low']
+    ema = EMA(dataHL, period)
+    dema = DEMA(dataHL, period)
+    result = []
+
+    for i in range(len(dataHL)):
+        result.append(ema[i] / dema[i])
+    return result
+
+
+# Индикатор волатильности Чайкина
+# https://equity.today/indikator-volatilnosti-chajkina.html
+def CHV(data, period):
+    dataHL = data['High'] - data['Low']
+    ema = EMA(dataHL, period)
+    result = []
+
+    for i in range(len(dataHL) - period):
+        result.append((ema[i + period] - ema[i]) / ema[i] * 100)
+    return result
---
+++ CORE/Plots.py
@@ -0,0 +1,336 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from CORE.Indicators import *
+from DATA.tradeBot_parser_static import get_quotes_tab
+import datetime as dt
+
+
+# я обозначил дата за 0, тк не обозначать нельзя
+# мне ее оставить пустой, чтобы она как глобальная переменная была,или добавить как аргумент?
+
+# параметр n отвечает за покупку - продажу акций
+# если n = 2 - покупать
+#      n = 1 - ничего
+#      n = 0 - продавать
+
+
+def plot_SMA(lenData, data, risk=2):
+    if risk == 1:
+        window = 100  # низкий риск
+    elif risk == 3:  # высокий риск
+        window = 10
+    else:  # средний риска
+        window = 50
+    sma = SMA(data['Close'], window)
+
+    sma = sma[(-lenData):]
+    data = data[(-lenData):]
+
+    if (data['Close'].values[-1] <= sma[-1] and data['Close'].values[-2] > sma[-2]):
+        n = 0
+    elif (data['Close'].values[-1] >= sma[-1] and data['Close'].values[-2] < sma[-2]):
+        n = 2
+    else:
+        n = 1
+
+    return n, \
+           lambda blank: blank.plot(data['Date'], sma), \
+           lambda blank: blank.plot(data['Date'], data['Close'])
+
+
+def plot_twoSMA(lenData, data, risk=2):
+    if risk == 1:  # низкий риск
+        window1 = 70
+        window2 = 140
+    elif risk == 3:  # высокий риск
+        window1 = 25
+        window2 = 50
+    else:  # средний риска
+        window1 = 5
+        window2 = 10
+
+    smaShort = SMA(data['Close'], window1)
+    smaLong = SMA(data['Close'], window2)
+
+    smaShort = smaShort[(-lenData):]
+    smaLong = smaLong[(-lenData):]
+    data = data[(-lenData):]
+
+    if (smaShort[-1] <= smaLong[-1] and smaShort[-2] > smaLong[-2]):
+        n = 0
+    elif (smaShort[-1] >= smaLong[-1] and smaShort[-2] < smaLong[-2]):
+        n = 2
+    else:
+        n = 1
+
+    return n, \
+           lambda blank: blank.plot(data['Date'], smaShort), \
+           lambda blank: blank.plot(data['Date'], smaLong), \
+           lambda blank: blank.plot(data['Date'], data['Close'])
+
+
+def plot_EMA(lenData, data, risk=2):
+    if risk == 1:
+        window = 100  # низкий риск
+    elif risk == 3:  # высокий риск
+        window = 10
+    else:  # средний риска
+        window = 50
+
+    ema = EMA(data['Close'], window)
+
+    ema = ema[(-lenData):]
+    data = data[(-lenData):]
+
+    if (data['Close'].values[-1] <= ema[-1] and data['Close'].values[-2] > ema[-2]):
+        n = 0
+    elif (data['Close'].values[-1] >= ema[-1] and data['Close'].values[-2] < ema[-2]):
+        n = 2
+    else:
+        n = 1
+
+    return n, \
+           lambda blank: blank.plot(data['Date'], ema), \
+           lambda blank: blank.plot(data['Date'], data['Close'])
+
+
+def plot_DEMA(lenData, data, risk=2):
+    if risk == 1:
+        window = 100  # низкий риск
+    elif risk == 3:  # высокий риск
+        window = 10
+    else:  # средний риска
+        window = 50
+
+    dema = DEMA(data['Close'], window)
+
+    dema = dema[(-lenData):]
+    data = data[(-lenData):]
+
+    if (data['Close'].values[-1] <= dema[-1] and data['Close'].values[-2] > dema[-2]):
+        n = 0
+    elif (data['Close'].values[-1] >= dema[-1] and data['Close'].values[-2] < dema[-2]):
+        n = 2
+    else:
+        n = 1
+
+    return n, \
+           lambda blank: blank.plot(data['Date'], dema), \
+           lambda blank: blank.plot(data['Date'], data['Close'])
+
+
+def plot_TEMA(lenData, data, risk=2):
+    if risk == 1:
+        window = 100  # низкий риск
+    elif risk == 3:  # высокий риск
+        window = 10
+    else:  # средний риска
+        window = 50
+
+    tema = TEMA(data['Close'], window)
+
+    tema = tema[(-lenData):]
+    data = data[(-lenData):]
+
+    if (data['Close'].values[-1] <= tema[-1] and data['Close'].values[-2] > tema[-2]):
+        n = 0
+    elif (data['Close'].values[-1] >= tema[-1] and data['Close'].values[-2] < tema[-2]):
+        n = 2
+    else:
+        n = 1
+
+    return n, \
+           lambda blank: blank.plot(data['Date'], tema), \
+           lambda blank: blank.plot(data['Date'], data['Close'])
+
+
+def plot_RSI(lenData, data, risk=2):
+    if risk == 1:
+        window = 100  # низкий риск
+    elif risk == 3:  # высокий риск
+        window = 10
+    else:  # средний риска
+        window = 50
+
+    rsi = RSI(data['Close'], window)
+
+    rsi = rsi[(-lenData):]
+    data = data[(-lenData):]
+
+    if rsi[-1] > 70:
+        n = 0
+    elif rsi[-1] < 30:
+        n = 2
+    else:
+        n = 1
+
+    return n, \
+           lambda blank: blank.plot(data['Date'], rsi), \
+           lambda blank: blank.plot(data['Date'], [30] * len(data['Close'])), \
+           lambda blank: blank.plot(data['Date'], [70] * len(data['Close']))
+
+
+def plot_MACD(lenData, data, risk=1):
+    if risk == 1:  # низкий риск
+        shortwindow = 12
+        longwindow = 26
+        signalwindow = 9
+    else:  # высокий риск
+        shortwindow = 5
+        longwindow = 35
+        signalwindow = 5
+
+    macd_all = MACD(data['Close'], shortwindow, longwindow, signalwindow)
+
+    macd = macd_all[0]
+    macdsignal = macd_all[1]
+    macdhist = macd_all[2]
+
+    macd = macd[(-lenData):]
+    macdsignal = macdsignal[(-lenData):]
+    macdhist = macdhist[(-lenData):]
+    data = data[(-lenData):]
+
+    if (macdhist[-1] <= macdsignal[-1] and macdhist[-2] > macdsignal[-2]):
+        n = 0
+    elif (macdhist[-1] >= macdsignal[-1] and macdhist[-2] < macdsignal[-2]):
+        n = 2
+    else:
+        n = 1
+
+    return n, \
+           lambda blank: blank.plot(data['Date'], macdsignal, zorder=3), \
+           lambda blank: blank.bar(data['Date'], macdhist, zorder=2), \
+           lambda blank: blank.bar(data['Date'], macd, zorder=1)
+
+
+def plot_bulls(lenData, data, risk=2):
+    if risk == 1:
+        window = 100  # низкий риск
+    elif risk == 3:  # высокий риск
+        window = 10
+    else:  # средний риска
+        window = 50
+
+    bulls = Bulls_power(data, window)
+
+    bulls = bulls[(-lenData):]
+    data = data[(-lenData):]
+
+    if (bulls.values[-1] < bulls.values[-2] and bulls.values[-1] > 0 and bulls.values[-2] > 0):
+        n = 0
+    else:
+        n = 1
+
+    return n, \
+           (lambda blank: blank.bar(data['Date'], bulls)), \
+           (lambda blank: blank.plot(data['Date'], data['Close']))
+
+
+def plot_bears(lenData, data, risk=2):
+    if risk == 1:
+        window = 100  # низкий риск
+    elif risk == 3:  # высокий риск
+        window = 10
+    else:  # средний риска
+        window = 50
+
+    bears = Bears_power(data, window);
+
+    bears = bears[(-lenData):]
+    data = data[(-lenData):]
+
+    if (bears.values[-1] > bears.values[-2] and bears.values[-1] < 0 and bears.values[-2] < 0):
+        n = 2
+    else:
+        n = 1
+
+    return n, \
+           (lambda blank: blank.bar(data['Date'], bears)), \
+           (lambda blank: blank.plot(data['Date'], data['Close']))
+
+
+def plot_ER(lenData, data, risk=2):
+    if risk == 1:
+        window = 100  # низкий риск
+    elif risk == 3:  # высокий риск
+        window = 10
+    else:  # средний риска
+        window = 50
+
+    ema = Elder_Rays(data, window)[0]
+    bulls = Elder_Rays(data, window)[1]
+    bears = Elder_Rays(data, window)[2]
+
+    ema = ema[(-lenData):]
+    bears = bears[(-lenData):]
+    bulls = bulls[(-lenData):]
+    data = data[(-lenData):]
+
+    if (bears.values[-1] > bears.values[-2] and bears.values[-1] < 0 and bears.values[-2] < 0):
+        n = 2
+    elif (bulls.values[-1] < bulls.values[-2] and bulls.values[-1] > 0 and bulls.values[-2] > 0):
+        n = 0
+    else:
+        n = 1
+
+    return n, \
+           (lambda blank: blank.plot(data['Date'], ema)), \
+           (lambda blank: blank.bar(data['Date'], bulls)), \
+           (lambda blank: blank.bar(data['Date'], bears)), \
+           (lambda blank: blank.plot(data['Date'], data['Close']))
+
+
+def plot_MI(lenData, data):
+    mi = MI(data, 9)
+
+    result = []
+    for i in range(len(mi) - 49):
+        result.append(sum(mi[i + 25:i + 50]))
+
+    mi = result
+
+    mi = mi[(-lenData):]
+    data = data[(-lenData):]
+
+    mislice = mi[-20:-1]
+    flag = 0
+
+    for i in range(len(mislice)):
+        if (mi[i] > 26.5 and mi[i] < 27):
+            flag = 1
+
+    if (flag == 1 and mi[-1] < 26.5):
+        n = 2
+    else:
+        n = 1
+
+    return n, \
+           lambda blank: blank.plot(data['Date'], mi), \
+           lambda blank: blank.plot(data['Date'], [27] * len(data['Close'])), \
+           lambda blank: blank.plot(data['Date'], [26.5] * len(data['Close']))
+
+
+def plot_CHV(lenData, data, risk=2):
+    if risk == 1:
+        window = 100  # низкий риск
+    elif risk == 3:  # высокий риск
+        window = 10
+    else:  # средний риска
+        window = 50
+
+    chv = CHV(data, window)
+
+    chv = chv[(-lenData):]
+    data = data[(-lenData):]
+
+    if all(chv[i - 1] <= chv[i] for i in range(10)):
+        n = 2
+    elif all(chv[i - 1] >= chv[i] for i in range(10)):
+        n = 0
+    else:
+        n = 1
+
+    return n, \
+           (lambda blank: blank.plot(data['Date'], chv)), \
+           (lambda blank: blank.plot(data['Date'], data['Close']))
