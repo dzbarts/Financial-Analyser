c069e1cdd904a73c1252f1bf1a9200b8db7eb795
Artem <dzbarts@mail.ru>
2021-09-23 21:59:27 +0300

    add CAPA index Pandas DataFrame and recommendation function

--- CORE/Countries.py
+++ CORE/Countries.py
@@ -1,8 +1,4 @@
-#https://www.starcapital.de/fileadmin/user_upload/files/publikationen/Kapitalmarktforschung/2020-04_KMF_Update_EN_www.html
-#https://vse-dengy.ru/pro-investitsii/fondovye-koeffitsienty.html
-#https://smart-lab.ru/blog/562256.php
-#https://journal.tinkoff.ru/cape/
-
+import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
 
@@ -28,3 +24,23 @@ for i in range(len(portfolio)):
 fig1, ax1 = plt.subplots()
 ax1.pie(weight, labels=countries)
 plt.show()
+
+# CAPA index Pandas DataFrame
+# index was taken on 6/30/21
+tcapa = pd.DataFrame({'Nation': ['Canada', 'US', 'UK', 'Italy', 'Spain', 'Russia',
+                                 'India', 'Japan', 'China', 'Hong Kong', 'Australia'],
+                      'Calculated Using': ['S&P/TSX Composite', 'S&P', 'FTSE', 'FTSE MIB',
+                                           'IBEX', 'MOEX', 'NIFTY', 'All public companies, (uncons.)',
+                                           'SSE Composite', 'Hang Seng', 'ASX All Ordinaries'],
+                      'Index': [25.49, 37.73, 15.05, 24.17, 18.22, 10.96, 33.38, 33.16, 18.43, 14.62, 19.83]
+                      })
+
+
+# recommendation function
+def cape_advice():
+    if max(weight) / sum(weight) >= 0.4 and \
+            countries[weight.index(max(weight))] in \
+            ['Canada', 'US', 'Italy', 'India', 'Japan']:
+        return "It is recommended to invest in stocks of countries such as Russia, UK or China"
+    else:
+        return "Diversification by country according to CAPE is quite good"
