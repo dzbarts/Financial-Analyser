14da606168db8867c948a5b6f9dc7bc4b8ca11cf
Artem <dzbarts@mail.ru>
2021-09-23 19:11:42 +0300

    add tab portfolio

---
+++ CORE/Portfolio.py
@@ -0,0 +1,48 @@
+import yfinance as yf
+
+port = []
+
+msft = yf.Ticker('MSFT')
+aapl = yf.Ticker('AAPL')
+tsla = yf.Ticker('TSLA')
+yndx = yf.Ticker('YNDX')
+
+ticker = ['AFLT.ME','IMOEX.ME']
+stock = yf.download(ticker)
+
+# Выделение скорректированой цены закрытия
+all_adj_close = stock[['Adj Close']]
+#print(all_adj_close)
+#print(stock[['Sector']])
+
+portfolio = [msft, aapl, tsla, yndx]
+
+# dividends = []
+#
+# for i in range(len(portfolio)):
+#     el = portfolio[i].dividends
+#     dividends.append(el)
+#
+# print(dividends)
+
+msft1 = yf.download('MSFT', period='1d')
+aapl1 = yf.download('AAPL', period='1d')
+tsla1 = yf.download('TSLA', period='1d')
+yndx1 = yf.download('YNDX', period='1d')
+
+
+value = 0
+port = [msft1, aapl1, tsla1, yndx1]
+
+for i in range(4):
+    value += port[i].Close
+print(round(float(value),2))
+
+
+wallet = 10000
+
+spent = 0
+
+
+
+
