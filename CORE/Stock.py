fd225ceb594ac7ad6cd67b4ce5f1abff5e87bd7e
Artem <dzbarts@mail.ru>
2021-11-08 17:53:29 +0300

    some code to functions

--- CORE/Stock.py
+++ CORE/Stock.py
@@ -1,15 +1,15 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
-import numpy as np
-from Portfolio import *
 import matplotlib
 
-plt.style.use('seaborn')
 
-stck = yf.Ticker('MSFT')
+def get_stock(stock):
+    return yf.Ticker(stock)
+
 
 def plot_stock(stock, period):  # передаю не только период, но и акцию,
     # чтобы можно было выводить график по определенной акции
+    plt.style.use('seaborn')
     fig_st, ax1 = plt.subplots()
     ax1.plot(yf.download(stock).Close[-period:])
     ax1.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%d/%m"))
@@ -19,38 +19,50 @@ def plot_stock(stock, period):  # передаю не только период,
 
 
 # show actions (dividends, splits)
-stck_actions = stck.actions
-stck_actions = stck_actions.reset_index()
+def get_stock_actions(stock):
+    stock_actions = stock.actions
+    stock_actions = stock_actions.reset_index()
+    return stock_actions
 
 
 # show quarterly_financials
-stck_quarterly_financials = stck.quarterly_financials
-stck_quarterly_financials = stck_quarterly_financials.rename_axis('Quarterly financials')
-stck_quarterly_financials = stck_quarterly_financials.reset_index()
+def get_stock_quarterly_financials(stock):
+    stock_quarterly_financials = stock.quarterly_financials
+    stock_quarterly_financials = stock_quarterly_financials.rename_axis('Quarterly financials')
+    stock_quarterly_financials = stock_quarterly_financials.reset_index()
+    return stock_quarterly_financials
 
 
 # show quarterly_balance_sheet
-stck_quarterly_balance_sheet = stck.quarterly_balance_sheet
-stck_quarterly_balance_sheet = stck_quarterly_balance_sheet.rename_axis('Quarterly balance sheet')
-stck_quarterly_balance_sheet = stck_quarterly_balance_sheet.reset_index()
+def get_stock_quarterly_balance_sheet(stock):
+    stock_quarterly_balance_sheet = stock.quarterly_balance_sheet
+    stock_quarterly_balance_sheet = stock_quarterly_balance_sheet.rename_axis('Quarterly balance sheet')
+    stock_quarterly_balance_sheet = stock_quarterly_balance_sheet.reset_index()
+    return stock_quarterly_balance_sheet
 
 
 # show quarterly_cashflow
-stck_quarterly_cashflow = stck.quarterly_cashflow
-stck_quarterly_cashflow = stck_quarterly_cashflow.rename_axis('Quarterly cashflow')
-stck_quarterly_cashflow = stck_quarterly_cashflow.reset_index()
-
+def get_stock_quarterly_cashflow(stock):
+    stock_quarterly_cashflow = stock.quarterly_cashflow
+    stock_quarterly_cashflow = stock_quarterly_cashflow.rename_axis('Quarterly cashflow')
+    stock_quarterly_cashflow = stock_quarterly_cashflow.reset_index()
+    return stock_quarterly_cashflow
 
 # show quarterly_earnings
-stck_quarterly_earnings = stck.quarterly_earnings
-stck_quarterly_earnings = stck_quarterly_earnings.reset_index()
+def get_stock_quarterly_earnings(stock):
+    stock_quarterly_earnings = stock.quarterly_earnings
+    stock_quarterly_earnings = stock_quarterly_earnings.reset_index()
+    return stock_quarterly_earnings
 
 
 # show sustainability
-stck_sustainability = stck.sustainability
-stck_sustainability = stck_sustainability.reset_index()
+def get_stock_sustainability(stock):
+    stock_sustainability = stock.sustainability
+    stock_sustainability = stock_sustainability.reset_index()
+    return stock_sustainability
 
 
 # show ISIN code - *experimental*
 # ISIN = International Securities Identification Number
-stck_isin = stck.isin
+def get_stock_isin(stock):
+    return stock.isin
